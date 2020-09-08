08edf1554331bff6049781a6bec4665adf7a2562
culiutudousi <chenzhicheng1994@gmail.com>
2020-09-07 23:44:35 -0700

    e4

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,17 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array == null) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+
+  function ListNode(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+
+  return array.reverse().reduce((acc, curr) => new ListNode(curr, acc), null);
 }
